1f8d25ed5118fbb153e672c55a6d62d3c6b5e174
Alejandra Estefanía Martínez Muñoz <alejandra.martinez6730@alumnos.udg.mx>
2023-01-26 17:50:57 -0600

    Create Actividad3.c
    La cosa esa para no sé q

---
+++ Actividad3.c
@@ -0,0 +1,23 @@
+/*******************PRESENTACIÓN*******************
+NOMBRE: Alejandra Estefanía Martínez Muñoz
+FECHA: 25 de enero de 2023
+PROGRAMA: actividad3.cpp
+CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+INGENIERÍA EN COMPUTACIÓN / 2DO SEMESTRE
+PROFESOR: Carlos Javier Cruz Franco
+DESCRIPCIÓN: Elaborar programa donde se le pida un núm. al usuario.
+**************************************************/ 
+#include <stdio.h>
+
+int main () { //Programa principal
+
+	//Definición de variables
+	int a;
+
+	//Solicitud de datos
+	printf ( "Bienvenido al programa :)\n" "Inserte un numero ---> ");
+	scanf ("%i", &a);
+	
+	
+	return 0;
+}
