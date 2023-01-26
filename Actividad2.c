23d6b1926ce542fcce875152573649d0c082556d
Alejandra Estefanía Martínez Muñoz <alejandra.martinez6730@alumnos.udg.mx>
2023-01-26 16:14:08 -0600

    Create Actividad2.c
    Pal domongo

---
+++ Actividad2.c
@@ -0,0 +1,24 @@
+/*******************PRESENTACIÓN*******************
+NOMBRE: Alejandra Estefanía Martínez Muñoz
+FECHA: 25 de enero de 2023
+PROGRAMA: actividad2.cpp
+CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+INGENIERÍA EN COMPUTACIÓN / 2DO SEMESTRE
+PROFESOR: Carlos Javier Cruz Franco
+DESCRIPCIÓN: 
+**************************************************/ 
+#include <stdio.h>
+
+int main () { //Programa principal
+
+	//Definición de variables
+	int a;
+
+	//Solicitud de datos
+	printf( "Hola, qué tal? :)\n" );
+	printf( "cuantos anos tienes?\n" );
+	scanf( "%d", &a );
+	printf( "puto\a");
+	
+	return 0;
+}
