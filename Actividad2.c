9b3f2b74fb4f23e7d66fd41033027f03a73174fb
Alejandra Estefanía Martínez Muñoz <123692081+alex-fany@users.noreply.github.com>
2023-01-29 12:07:33 -0600

    Update Actividad2.c
    AA

--- Actividad2.c
+++ Actividad2.c
@@ -8,19 +8,32 @@ PROFESOR: Carlos Javier Cruz Franco
 DESCRIPCIÓN: Crear programa usando caracteres de escape y cadenas de control
 **************************************************/ 
 #include <stdio.h> //Agrega librería
+#include <string.h>
 
 int main () { //Programa principal
 
 	//Definición de variables
-	int a, b;
+	int a, b, c, d;
+	char re[2], name[15];
 
-	//Solicitud de datos
-	printf( "Hola, qué tal\? :)\n" );
-	printf( "cuantos anos tienes?\n" );
+	//Solicitud de datos y demás
+	printf( "Hola, ¿qué tal\? :)\n" );
+	printf( "¿cuantas manos tienes?\n" );
 	scanf( "%d", &a );
-	printf( "qué muchos\a \n");
-	printf( "Esta línea no se va a ver porque le puse return\r");
-	printf( "pon tu año de nacimiento y el año actual y te diré cuantos años tienes\n");
-	scanf( "%
-	return 0;
+	printf( "qué muchas\a \n");
+	printf( "Esta línea no se va a ver\r");
+	printf( "pon dos anos y te dire cuantos anos cumples\r \nano de nacimiento:\n");
+	scanf( "%i", &b );
+	printf("\nano actual:\n");
+	scanf( "%i", &c );
+	d=c-b;
+	printf("\n tienes: %u", d);
+	printf("\n le atine\?\n");
+	scanf("%s", &re);
+	printf("¿Y cómo te llamas\?\n");
+	scanf("%s", &name);
+	printf("\n Entonces\a\n te llamas, %s", name);
+	printf("\n\a tienes, %u", d);
+	printf("\n mira un cuadrado\a\a\a\a\a\a\a\a\a\a\a\a\a\n *********\n *\t *\n *\t *\n *\t *\n *\t *\n *********\n");
+	return 0; //Termina programa
 }
